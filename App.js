1e441127fb57b0fb82121d95faffd63b69f1e065
Bhushan Bobhate <bobhatebhushan@gmail.com>
2023-07-16 22:18:43 +0530

    react episode 02

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const divElement = React.createElement('div', {id: 'parent'}, 
     React.createElement('div', {id: 'child1'},
-    [React.createElement('h1', {}, 'I am h1 tag'), React.createElement('h1', {}, 'I am h1 tag')]),
+    [React.createElement('h1', {}, 'This is React by Bhushan Bobhate'), React.createElement('h1', {}, 'I am h1 tag')]),
     React.createElement('div', {id: 'child2'}, 
-    [React.createElement('h2', {}, 'I am h1 tag'), React.createElement('h2', {}, 'I am h1 tag')])
+    [React.createElement('h2', {}, 'I am h2 tag'), React.createElement('h2', {}, 'I am h2 tag')])
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 React Training Episodes
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching Faster Builds (.parcel-cache)
+- Image Optimization
+- Minifaction
+- Bundling
+- Compress
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - support older brwosers
+- Diagnostic
+- Error Handling
+- Support HTTPS
+- Tree Shaking (remove unused code)
+- Different build for dev & prod
