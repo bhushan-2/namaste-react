ed49ea7aabe778ba3f89dbc79f96036323919eaa
Bhushan Bobhate <bobhatebhushan@gmail.com>
2023-07-27 11:19:52 +0530

    react episode 08

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,33 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <h2>This is Namaste React Web Series</h2>
-    </div>
-  );
-};
+import User from "./User";
+import UserClass from "./UserClass";
+import React from "react";
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent constructor method");
+  }
+
+  componentDidMount() {
+    console.log('Parent didMount called');
+  }
+
+  render() {
+    console.log("Parent render method");
+    const userObj = {
+      name: "Bhushan Bobhate",
+      location: "Kokan",
+      handle: "twitter@123",
+    };
+
+    return (
+      <div>
+        <h1>About Us</h1>
+        <h2>This is Namaste React Web Series</h2>
+        <UserClass userDetails={{name: 'First', location: 'Kokan', handle:'twitter@123'}} />
+      </div>
+    );
+  }
+}
 
 export default About;
